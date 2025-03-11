fe7dfff0a51241efb57a89d2539de48112daa8c8
Mr.Tree <ripper-s@ripper-s.com>
2025-03-11 14:38:32 +0800

    4.0.1

--- BTAS.js
+++ BTAS.js
@@ -2,7 +2,7 @@
 // @name         BTAS
 // @namespace    https://github.com/Mr-Tree-S/BTAS
 // @homepageURL  https://github.com/Mr-Tree-S/BTAS
-// @version      3.9.9
+// @version      4.0.1
 // @description  Blue Team Auxiliary Script
 // @author       Barry, Jack, Xingyu, Mike
 // @license      Apache-2.0
